4b24154269236fd055e5d13292b061d204b81ebd
Jesse Horne <j.horne2796@gmail.com>
2020-09-29 16:36:54 -0500

    added ability to withdraw to deposit module

--- modules/deposit.js
+++ modules/deposit.js
@@ -3,9 +3,9 @@ const models = require('../models').db;
 
 var m = {};
 
-m.help = "`db deposit|dep X` - Deposits X from your wallet to your bank!";
-
-m.valids = ['deposit', 'dep'];
+m.help = "`db deposit|dep X` - Deposits X from your wallet to your bank!\n";
+m.help += "`db withdraw X` - Withdraw X from your bank to your wallet!";
+m.valids = ['withdraw'];
 
 m.valid = function(data) {
   if (!m.valids.includes(data[0])) {
@@ -26,24 +26,43 @@ m.handle = async function(data, user=null) {
     return false;
   }
 
-  // Get Amount
-  const amt = parseInt(msg[1]);
+  if (['deposit', 'dep'].includes(msg[0])) {
+    // Get Amount
+    const amt = parseInt(msg[1]);
 
-  if (amt > user.wallet) {
-    data.reply("You don't have that much in your wallet.");
-    return false;
+    if (amt > user.wallet) {
+      data.reply("You don't have that much in your wallet.");
+      return false;
+    }
+
+    if (amt <= 0) {
+      return false;
+    }
+
+    user.wallet -= amt;
+    user.bank += amt;
+
+    data.reply("You've deposited `$" + helpers.db.format_money(amt) + "` into your bank account!");
+  } else if (['withdraw'].includes(msg[0])) {
+    // Get Amount
+    const amt = parseInt(msg[1]);
+
+    if (amt > user.bank) {
+      data.reply("You don't have that much in your bank.");
+      return false;
+    }
+
+    if (amt <= 0) {
+      return false;
+    }
+
+    user.wallet += amt;
+    user.bank -= amt;
+
+    data.reply("You've withdrawn `$" + helpers.db.format_money(amt) + "` into your wallet!");
   }
 
-  if (amt <= 0) {
-    return false;
-  }
-
-  user.wallet -= amt;
-  user.bank += amt;
   await user.save();
-
-  data.reply("You've deposited `$" + amt + "` into your bank account!");
-
 
   return true;
 }
